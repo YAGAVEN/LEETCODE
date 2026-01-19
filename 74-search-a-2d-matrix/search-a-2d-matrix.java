97cb0f8d397cdbb3eb3dcf3c4f57c57072bf167b
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-01-19 12:05:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (49.72%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -2,20 +2,22 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int tar) {
         int n = matrix.length;
         int m = matrix[0].length;
-        for(int i=0;i<n;i++){
-            if(matrix[i][0] <= tar && matrix[i][m-1]>= tar){
-                return(binary(matrix[i],tar));
+        int low = 0,high=n * m-1;
+        int mid = 0,row=0,col=0;
+        while(low <= high){
+            mid = (low + high)/2;
+            row = mid/m;
+            col=mid%m;
+            if(matrix[row][col]==tar){
+                return true;
+            }
+            else if(matrix[row][col] > tar){
+               high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
         return false;
         
-    }
-    public boolean binary(int[] mat, int tar){
-        for(int i : mat){
-            if(i==tar){
-                return true;
-            }
-        }
-        return false;
     }
 }
