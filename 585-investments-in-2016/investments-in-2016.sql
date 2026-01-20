054bd7d60b7cac423a7f760fe574b86e10927196
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-01-20 21:34:28 +0530

    Time: 554 ms (42.25%) | Memory: 0B (100.00%) - LeetSync

---
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -0,0 +1,14 @@
+SELECT ROUND(SUM(tiv_2016), 2) AS TIV_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+)
+AND CONCAT(LAT, ',', LON) IN (
+    SELECT CONCAT(LAT, ',', LON)
+    FROM Insurance
+    GROUP BY LAT, LON
+    HAVING COUNT(*) = 1
+);
