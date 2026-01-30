a970ff3f01e430f187c78c92895e99dcd29efefc
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-01-30 21:11:43 +0530

    Time: 0 ms (100.00%) | Memory: 46.9 MB (22.82%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -5,21 +5,16 @@ class Solution {
 
         while (fast != null && fast.next != null) {
             slow = slow.next;
-
             fast = fast.next.next;
-
             if (slow == fast) {
                 slow = head;
-
                 while (slow != fast) {
                     slow = slow.next;
                     fast = fast.next;
                 }
-
                 return slow;
             }
         }
-
         return null;
     }
 }
