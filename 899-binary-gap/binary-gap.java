8909b455377db0bb4762a13a007e607472fc96fa
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-02-22 08:57:00 +0530

    Time: 1 ms (40.87%) | Memory: 42.2 MB (67.94%) - LeetSync

---
+++ 899-binary-gap/binary-gap.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int binaryGap(int n) {
+        String str = Integer.toBinaryString(n);
+        int ma = 0;
+        int i=0;
+        while(i<str.length()){
+            if(str.charAt(i)=='1')break;
+            i++;
+        }
+        int j=i+1;
+        while(i<str.length() && j<str.length()){
+            if(str.charAt(i) == '1' && str.charAt(j)== '1' ){
+                int d = j-i;
+                if(d>ma){
+                    ma=d;
+                }
+                i=j;
+            }
+            j++;
+        }
+        return ma;
+    }
+}
