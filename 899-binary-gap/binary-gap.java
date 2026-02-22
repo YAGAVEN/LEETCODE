5f3655065d4f7f061d2ca190db820d19f8c4468d
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-02-22 08:58:06 +0530

    Time: 1 ms (40.87%) | Memory: 42.5 MB (30.35%) - LeetSync

--- 899-binary-gap/binary-gap.java
+++ 899-binary-gap/binary-gap.java
@@ -3,10 +3,10 @@ class Solution {
         String str = Integer.toBinaryString(n);
         int ma = 0;
         int i=0;
-        while(i<str.length()){
-            if(str.charAt(i)=='1')break;
-            i++;
-        }
+        // while(i<str.length()){
+        //     if(str.charAt(i)=='1')break;
+        //     i++;
+        // }
         int j=i+1;
         while(i<str.length() && j<str.length()){
             if(str.charAt(i) == '1' && str.charAt(j)== '1' ){
