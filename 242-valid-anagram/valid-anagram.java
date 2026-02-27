145ae1ea1a9b465aa9cb809470fe215afe7ce83a
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-02-27 17:53:41 +0530

    Time: 17 ms (16.99%) | Memory: 46.7 MB (15.47%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,30 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length())return false;
-        char[] sa = s.toCharArray();
-        char[] ta = t.toCharArray();
-        Arrays.sort(sa);
-        Arrays.sort(ta);
-        s = Arrays.toString(sa);
-        t = Arrays.toString(ta);
-        return s.equals(t);
+        // char[] sa = s.toCharArray();
+        // char[] ta = t.toCharArray();
+        // Arrays.sort(sa);
+        // Arrays.sort(ta);
+        // s = Arrays.toString(sa);
+        // t = Arrays.toString(ta);
+        // return s.equals(t);
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(t.charAt(i))){
+                map.put(t.charAt(i),map.get(t.charAt(i))-1);
+            }else{
+                return false;
+            }
+        }
+        for(int i : map.values()){
+            if(i<0){
+                return false;
+            }
+        }
+        return true;
         
     }
 }
