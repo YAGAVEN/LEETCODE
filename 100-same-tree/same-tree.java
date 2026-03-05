e4bd25eda692a5352562a0e2abb3176bbc3c7243
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-03-05 06:39:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (13.67%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -22,15 +22,12 @@ class Solution {
         while(!q1.isEmpty() && !q2.isEmpty()){
             TreeNode t1 = q1.poll();
             TreeNode t2 = q2.poll();
-
-            
-            if(t1 == null && t2==null)continue;
-            if(t1 == null || t2==null)return false;
-            if(t1.val != t2.val)return false;
-
+            if(t1==null && t2==null)continue;
+            if(t1==null || t2==null)return false;
+            if(t1.val != t2.val) return false;
             q1.offer(t1.left);
-            q2.offer(t2.left);
             q1.offer(t1.right);
+            q2.offer(t2.left);
             q2.offer(t2.right);
         }
         return q1.isEmpty()&&q2.isEmpty();
