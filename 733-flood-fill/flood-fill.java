50583e0c60115f5cdc04b78bc1e310bb9f0267c5
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-03-05 10:43:17 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (80.78%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -2,18 +2,18 @@ class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int ori = image[sr][sc];
         if(ori==color)return image;
-
-        dfs(image,sr,sc,color,ori); 
-        return image;  
+        dsf(image,sr,sc,color,ori);
+        return image;    
     }
-    private void dfs(int[][] mat,int i,int j,int c,int ori){
+    private void dsf(int[][] mat,int i,int j,int c,int ori){
         if(i<0||j<0||i>=mat.length||j>=mat[0].length||mat[i][j]!=ori){
             return;
         }
         mat[i][j]=c;
-        dfs(mat,i,j-1,c,ori);
-        dfs(mat,i,j+1,c,ori);
-        dfs(mat,i-1,j,c,ori);
-        dfs(mat,i+1,j,c,ori);
+        dsf(mat,i+1,j,c,ori);
+        dsf(mat,i-1,j,c,ori);
+        dsf(mat,i,j+1,c,ori);
+        dsf(mat,i,j-1,c,ori);
+
     }
 }
