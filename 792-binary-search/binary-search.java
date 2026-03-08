5ca694b5f448646417605373a4712e9d67b07666
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-03-08 15:23:23 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (84.52%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,7 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return binarySearch(nums,target,0,nums.length-1);
-        
+        return binarySearch(nums,target,0,nums.length-1);    
     }
     public int binarySearch(int[] nums , int x,int left , int right){
         if(left > right)return -1;
