bbd200fec5682900a6a84926cc6166c528df4b52
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-03-08 18:47:36 +0530

    Time: 13 ms (19.49%) | Memory: 47 MB (63.57%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int singleNumber1(int[] nums) {
+    public int singleNumber(int[] nums) {
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i : nums){
             map.put(i,map.getOrDefault(i,0)+1);
@@ -11,7 +11,7 @@ class Solution {
         }
         return -1;  
     }
-        public int singleNumber(int[] nums) {
+        public int singleNumber1(int[] nums) {
         HashSet<Integer> map = new HashSet<>();
         for(int i : nums){
             if(map.contains(i)){
