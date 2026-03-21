d6cb441f898a5b067e7b980aab36141d75a5dc11
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-03-21 08:45:14 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (17.14%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
@@ -1,17 +1,15 @@
 class Solution {
-    public int[][] reverseSubmatrixYagaven(int[][] grid, int x, int y, int k) {
-        int n = grid.length;
-        int m = grid[0].length;
-        for(int i=x;i<((k-1)+x)/2;i++){
-            for(int j=y;j<((k-1)+y)/2;j++){
-                int temp = grid[i][j];
-                grid[i][j] = grid[(k-1)+i][j];
-                grid[(k-1)+i][j] = temp;
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+        for(int i=0;i<k/2;i++){
+            for(int j=0;j<k;j++){
+                int temp = grid[i+x][j+y];
+                grid[i+x][j+y] = grid[x + k - 1 - i][y+j];
+                grid[x + k - 1 - i][y+j] = temp;
             }
         }
         return grid;
     }
-        public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+        public int[][] reverseSubmatrixYaga(int[][] grid, int x, int y, int k) {
         for (int rowOffset = 0; rowOffset < k / 2; rowOffset++) {
             int top = x + rowOffset;
             int bottom = x + k - 1 - rowOffset;
