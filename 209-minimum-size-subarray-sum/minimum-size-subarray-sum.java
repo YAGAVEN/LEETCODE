32dba0538df4d86f9236dd41d3b02284c94432a5
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-03-22 21:30:17 +0530

    Time: 1 ms (99.84%) | Memory: 61.9 MB (82.44%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -14,4 +14,19 @@ class Solution {
         return (minSize==Integer.MAX_VALUE) ? 0 : minSize;
         
     }
+    public int minSubArrayLenBrute(int target ,int[] nums){
+        int n = nums.length;
+        int min  = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = i; j < n; j++){
+                sum += nums[j];
+                if(sum >= target){
+                    min = Math.min(min,j - i + 1);
+                    break;
+                }
+            }
+        }
+        return min==Integer.MAX_VALUE?0:min;
+    }
 }
