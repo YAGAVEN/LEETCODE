b45128a26e33598e03af2a5cc24fa32f9d52dca6
YAGAVEN S <145546189+YAGAVEN@users.noreply.github.com>
2026-04-07 14:42:42 +0530

    Time: 56 ms (100.00%) | Memory: 58 MB (40.56%) - LeetSync

--- 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
+++ 2178-walking-robot-simulation-ii/walking-robot-simulation-ii.java
@@ -44,11 +44,3 @@ private final int width;
       }
    }
 }
-
-/**
- * Your Robot object will be instantiated and called as such:
- * Robot obj = new Robot(width, height);
- * obj.step(num);
- * int[] param_2 = obj.getPos();
- * String param_3 = obj.getDir();
- */
